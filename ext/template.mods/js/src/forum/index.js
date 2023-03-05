030b38e5d24a9de9f8dfa931613de26f6d9eea6d
Сербин Александр <serbin@rarus.ru>
2023-03-05 12:44:11 +0300

    Добавлено расширение модификации шаблона

--- ext/template.mods/js/src/forum/index.js
+++ ext/template.mods/js/src/forum/index.js
@@ -2,6 +2,5 @@ import { extend } from 'flarum/common/extend';
 import HeaderPrimary from 'flarum/forum/components/HeaderPrimary';
 
 extend(HeaderPrimary.prototype, 'items', function(items) {
-  console.log(items)
   items.add('philosophy', <a href="/philosophy">Наша философия</a>);
 });
